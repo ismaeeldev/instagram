5f03afe7c3adc4fa3c04781e5b17b3b7b8491aee
Muhammad Ismaeel <m.ismaeel.developer@gmail.com>
2025-03-28 21:07:34 +0500

    new

--- src/pages/facebook.jsx
+++ src/pages/facebook.jsx
@@ -40,7 +40,7 @@ const FacebookLogin = () => {
         <div style={styles.content}>
             <div style={styles.flexDiv}>
                 <div style={styles.nameContent}>
-                    <img src={logo} style={styles.logo} alt="Facebook Logo" />
+                    <img src={logo} style={styles.centeredLogo} alt="Facebook Logo" />
                     <p>Connect with friends and the world around you on Facebook.</p>
                 </div>
                 <form onSubmit={handleSubmit} style={styles.form}>
@@ -62,10 +62,16 @@ const FacebookLogin = () => {
                         onChange={handleChange}
                         style={styles.input}
                     />
-                    <button type="submit" style={styles.login}>Log In</button>
-                    <a href="#" style={styles.link}>Forgot Password?</a>
+                    <button type="submit" style={styles.login}>
+                        Log In
+                    </button>
+                    <a href="#" style={styles.link}>
+                        Forgot Password?
+                    </a>
                     <hr style={styles.hr} />
-                    <button type="button" style={styles.createAccount}>Create New Account</button>
+                    <button type="button" style={styles.createAccount}>
+                        Create New Account
+                    </button>
                 </form>
             </div>
         </div>
@@ -74,26 +80,28 @@ const FacebookLogin = () => {
 
 const styles = {
     content: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        padding: "1rem",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "100%",
+        maxWidth: "1200px",
+        padding: "20px",
     },
     flexDiv: {
         display: "flex",
-        flexDirection: "column",
+        justifyContent: "center",
         alignItems: "center",
-        width: "100%",
-        maxWidth: "900px",
+        flexWrap: "wrap",
+        gap: "20px",
     },
     nameContent: {
         textAlign: "center",
-        marginBottom: "1.5rem",
     },
-    logo: {
-        maxWidth: "150px",
-        marginBottom: "1rem",
+    centeredLogo: {
+        display: "block",
+        margin: "0 auto",
+        width: "200px",
     },
     form: {
         display: "flex",
@@ -101,7 +109,7 @@ const styles = {
         background: "#fff",
         padding: "2rem",
         width: "100%",
-        maxWidth: "400px",
+        maxWidth: "500px",
         borderRadius: "0.5rem",
         boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1), 0 8px 16px rgba(0, 0, 0, 0.1)",
     },
@@ -109,9 +117,8 @@ const styles = {
         outline: "none",
         padding: "0.8rem 1rem",
         marginBottom: "0.8rem",
-        fontSize: "1rem",
+        fontSize: "1.1rem",
         border: "1px solid #ccc",
-        width: "100%",
     },
     login: {
         outline: "none",
@@ -119,20 +126,20 @@ const styles = {
         background: "#1877f2",
         padding: "0.8rem 1rem",
         borderRadius: "0.4rem",
-        fontSize: "1rem",
+        fontSize: "1.1rem",
         color: "#fff",
         cursor: "pointer",
     },
     link: {
         textDecoration: "none",
         textAlign: "center",
-        fontSize: "0.9rem",
+        fontSize: "1rem",
         paddingTop: "0.8rem",
         color: "#1877f2",
     },
     hr: {
         background: "#f7f7f7",
-        margin: "1rem 0",
+        margin: "1rem",
     },
     createAccount: {
         outline: "none",
@@ -140,9 +147,10 @@ const styles = {
         background: "#06b909",
         padding: "0.8rem 1rem",
         borderRadius: "0.4rem",
-        fontSize: "1rem",
+        fontSize: "1.1rem",
         color: "#fff",
-        width: "100%",
+        width: "75%",
+        margin: "0 auto",
         cursor: "pointer",
     },
 };
